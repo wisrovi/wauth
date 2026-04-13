99a47320c5eb406603a2c05f527776e6b43c26af
williamrv-edt <138199553+williamrv-edt@users.noreply.github.com>
2026-04-13 14:07:20 +0200

    [CONFIG] Add Sphinx conf

---
+++ docs/conf.py
@@ -0,0 +1,128 @@
+# WAuth documentation build configuration file.
+#
+# This file only contains a subset of the possible configuration values.
+# For a full list, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+
+# -- Path setup --------------------------------------------------------------
+# Add the project root to the Python path
+sys.path.insert(0, os.path.abspath(".."))
+
+# -- Project information -----------------------------------------------------
+
+project = "WAuth"
+copyright = "2026, William Rodríguez"
+author = "William Rodríguez — wisrovi"
+
+# The full version, including alpha/beta/rc tags
+release = "1.5.3"
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx_copybutton",
+    "myst_parser",
+    "sphinx_design",
+    "sphinx_sitemap",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**/README.md"]
+
+# The suffix(es) of source filenames.
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
+
+# The master toctree document.
+master_doc = "index"
+
+# Default language
+language = "en"
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = "furo"
+html_title = "WAuth Documentation"
+html_short_title = "WAuth"
+
+html_theme_options = {
+    "light_logo": "logo.png",
+    "dark_logo": "logo.png",
+    "sidebar_hide_name": False,
+    "navigation_with_keys": True,
+    "source_repository": "https://github.com/wisrovi/wpipe/",
+    "source_branch": "main",
+    "source_directory": "docs/",
+}
+
+html_static_path = ["_static"]
+html_extra_path = ["_extra"]
+
+# URL which documentation lives at
+html_baseurl = "https://wpipe.readthedocs.io/en/latest/"
+
+# Generate sitemap
+sitemap_url_scheme = "{link}"
+
+# -- Options for autodoc -----------------------------------------------------
+
+autodoc_default_options = {
+    "members": True,
+    "member-order": "bysource",
+    "special-members": "__init__",
+    "undoc-members": True,
+    "exclude-members": "__weakref__",
+    "show-inheritance": True,
+}
+
+autodoc_typehints = "description"
+autodoc_typehints_description_target = "documented"
+
+# -- Options for napoleon ----------------------------------------------------
+
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = True
+napoleon_use_admonition_for_notes = True
+napoleon_use_admonition_for_references = True
+napoleon_use_ivar = True
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = True
+
+# -- Options for intersphinx -------------------------------------------------
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "pydantic": ("https://docs.pydantic.dev/latest/", None),
+}
+
+# -- Options for todo extension ----------------------------------------------
+
+todo_include_todos = True
+
+# -- Options for MyST parser -------------------------------------------------
+
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+    "html_admonition",
+]
