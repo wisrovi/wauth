8c3a70a26dabc2af1af63cca9117e5c5a88a6023
williamrv-edt <138199553+williamrv-edt@users.noreply.github.com>
2026-05-07 12:16:52 +0200

    chore: bump version to 0.5.0 in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ copyright = "2026, William Rodríguez"
 author = "William Rodríguez — wisrovi"
 
 # The full version, including alpha/beta/rc tags
-release = "0.3.1"
+release = "0.5.0"
 
 # -- General configuration ---------------------------------------------------
 
